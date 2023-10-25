4bab030016bca4fd0bf3db666fe9347354343ac0
Pranay Salve <64769068+pranaysalve@users.noreply.github.com>
2023-10-26 02:56:00 +0530

    cache update, delete based on event of adding post and deleting exiting

--- services/cache.js
+++ services/cache.js
@@ -5,17 +5,20 @@ const util = require("util");
 const redisURL = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisURL);
 
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || "");
+
   return this;
 };
 
 mongoose.Query.prototype.exec = async function () {
   if (!this.useCache) {
+    console.log("No cache available");
     return exec.apply(this, arguments);
   }
   const key = JSON.stringify(
@@ -24,8 +27,8 @@ mongoose.Query.prototype.exec = async function () {
     })
   );
 
-  const cacheValue = await client.get(key);
-
+  const cacheValue = await client.hget(this.hashKey, key);
+  console.log(cacheValue);
   if (cacheValue) {
     const doc = JSON.parse(cacheValue);
     return Array.isArray(doc)
@@ -34,8 +37,14 @@ mongoose.Query.prototype.exec = async function () {
   }
 
   const result = await exec.apply(this, arguments);
-  client.set(key, JSON.stringify(result));
+  client.hset(this.hashKey, key, JSON.stringify(result), "EX", 10);
 
   //   console.log({ result });
   return result;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  },
 };
